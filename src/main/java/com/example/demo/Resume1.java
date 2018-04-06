ede9fa6d6c7cb1ed6d587a97751f34d9a84a4ed8
Mesfin Admassu <medebay2003@yahoo.com>
2018-04-06 14:33:33 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Resume1.java
@@ -0,0 +1,125 @@
+package com.example.demo;
+
+public class Resume1 {
+    public Resume1() {
+    }
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String educationalAcheivements;
+    private String anotherdegree;
+    private String Experience;
+    private String otherexperiences;
+    private String jobtitle;
+    private String startdate;
+    private String enddate;
+    private String skillrating;
+    private String institution;
+
+
+
+    public String getOtherexperiences() {
+        return otherexperiences;
+    }
+
+    public void setOtherexperiences(String otherexperiences) {
+        this.otherexperiences = otherexperiences;
+    }
+
+
+    public String getAnotherdegree() {
+        return anotherdegree;
+    }
+
+    public void setAnotherdegree(String anotherdegree) {
+        this.anotherdegree = anotherdegree;
+    }
+
+
+    public String getJobtitle() {
+        return jobtitle;
+    }
+
+    public void setJobtitle(String jobtitle) {
+        this.jobtitle = jobtitle;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(String enddate) {
+        this.enddate = enddate;
+    }
+
+
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEducationalAcheivements() {
+        return educationalAcheivements;
+    }
+
+    public void setEducationalAcheivements(String educationalAcheivements) {
+        this.educationalAcheivements = educationalAcheivements;
+    }
+
+    public String getExperiences() {
+        return Experience;
+    }
+
+    public void setExperiences(String experience) {
+        Experience = experience;
+    }
+
+    public String getSkill() {
+        return skillrating;
+    }
+
+    public void setSkill(String skill) {
+        this.skillrating = skill;
+    }
+}
+
+
+
---
+++ src/main/java/com/example/demo/ResumeMain.java
@@ -0,0 +1,80 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ResumeMain {
+
+    public static void main(String[] args) {
+        boolean condition = false;
+        Scanner Scan = new Scanner(System.in);
+        Resume1 newresume;
+        String answer;
+        ArrayList<Resume1> resumes = new ArrayList<>();
+        do {
+            newresume = new Resume1();
+            System.out.println(resumes);
+
+            System.out.println("Enter your FirstName:");
+            newresume.setFirstName(Scan.nextLine());
+
+            System.out.println("Enter your LastName :");
+            newresume.setLastName(Scan.nextLine());
+
+            System.out.println("Enter your Email:");
+            newresume.setEmail(Scan.nextLine());
+
+            System.out.println("Enter your EducationalAcheivements:");
+            newresume.setEducationalAcheivements(Scan.nextLine());
+
+            System.out.println("Enter your institution:");
+            newresume.setInstitution(Scan.nextLine());
+
+            System.out.println("Enter your Another degree:");
+            newresume.setAnotherdegree(Scan.nextLine());
+
+            System.out.println("Enter your experiences:");
+            newresume.setExperiences(Scan.nextLine());
+
+            System.out.println("Enter your otherexperiences:");
+            newresume.setOtherexperiences(Scan.nextLine());
+
+            System.out.println("Enter your skill rating:");
+            newresume.setSkill(Scan.nextLine());
+
+            System.out.println("Enter your job title:");
+            newresume.setJobtitle(Scan.nextLine());
+
+            System.out.println("Enter your start date:");
+            newresume.setStartdate(Scan.nextLine());
+
+            System.out.println("Enter your end date:");
+            newresume.setEnddate(Scan.nextLine());
+
+
+
+            final boolean add = resumes.add(newresume);
+
+            System.out.println("Do you want enter another resume");
+            answer = Scan.nextLine();
+
+            if (answer.equalsIgnoreCase("no") || answer.
+                    equalsIgnoreCase("n")) {
+
+                condition = false;
+            } else {
+
+                condition = true;
+            }
+
+
+        } while (!condition);
+
+        for (Resume1 eachresume : resumes) {
+            System.out.println(eachresume.getFirstName());
+
+
+        }
+
+    }
+}
